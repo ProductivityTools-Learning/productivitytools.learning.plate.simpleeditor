99899415f056f701d529b3e0ea88f200f79492b2
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-04-08 07:43:29 +0200

    alignment

--- src/Components/EditorPage/index.tsx
+++ src/Components/EditorPage/index.tsx
@@ -1,4 +1,5 @@
 import {
+  PlateProvider,
   Plate,
   createPlugins,
   createBlockquotePlugin,
@@ -11,39 +12,64 @@ import {
   createStrikethroughPlugin,
   createUnderlinePlugin,
   TEditableProps,
-} from "@udecode/plate";
-import { MyParagraphElement, MyValue,MyPlatePlugin } from "./typescript/plateTypes";
-import { useState } from "react";
-import { basicElementsValue } from './basic-elements/basicElementsValue';
-import { basicMarksValue } from './basic-marks/basicMarksValue';
-import { plateUI } from './common/plateUI.ts';
-export default function EditorPage() {
-
-
-// try to remove a few plugins!
-const plugins = createPlugins<MyValue>(
-  [
-    createParagraphPlugin(),
-    createBlockquotePlugin(),
-    // createCodeBlockPlugin({
-    //   // You can either pass a component per plugin
-    //   component: CodeBlockElement,
-    // }),
-    createHeadingPlugin(),
-
-    createBoldPlugin(),
-    createItalicPlugin(),
-    createUnderlinePlugin(),
-    createStrikethroughPlugin(),
-    createCodePlugin(),
-  ],
-  {
-    // Or pass all components at once
-    components: plateUI,
-  }
-);
-
+  createAlignPlugin,
+  ELEMENT_H1,
+  ELEMENT_H2,
+  ELEMENT_H3,
+  ELEMENT_H4,
+  ELEMENT_H5,
+  ELEMENT_H6,
+  ELEMENT_PARAGRAPH,
   
+} from "@udecode/plate";
+import { MyParagraphElement, MyValue, MyPlatePlugin } from "./typescript/plateTypes";
+import { useState } from "react";
+import { basicElementsValue } from "./basic-elements/basicElementsValue";
+import { basicMarksValue } from "./basic-marks/basicMarksValue";
+import { plateUI } from "./common/plateUI.ts";
+import { AlignToolbarButtons } from "./align/AlignToolbarButtons.tsx";
+
+import { Toolbar } from "./toolbar/Toolbar.tsx";
+
+export default function EditorPage() {
+  // try to remove a few plugins!
+  const plugins = createPlugins<MyValue>(
+    [
+      createParagraphPlugin(),
+      createBlockquotePlugin(),
+      // createCodeBlockPlugin({
+      //   // You can either pass a component per plugin
+      //   component: CodeBlockElement,
+      // }),
+      createHeadingPlugin(),
+
+      createBoldPlugin(),
+      createItalicPlugin(),
+      createUnderlinePlugin(),
+      createStrikethroughPlugin(),
+      createCodePlugin(),
+      createAlignPlugin({
+        inject: {
+          props: {
+            validTypes: [
+              ELEMENT_PARAGRAPH,
+              ELEMENT_H1,
+              ELEMENT_H2,
+              ELEMENT_H3,
+              ELEMENT_H4,
+              ELEMENT_H5,
+              ELEMENT_H6
+            ]
+          }
+        }
+      }),
+    ],
+    {
+      // Or pass all components at once
+      components: plateUI,
+    }
+  );
+
   const initialValue = [
     {
       type: "p",
@@ -63,7 +89,16 @@ const plugins = createPlugins<MyValue>(
   return (
     <div>
       editorPage
-      <Plate plugins={plugins} editableProps={editableProps} onChange={(x) => setValue(JSON.stringify(x))} initialValue={initialValue} />
+      <PlateProvider<MyValue> initialValue={initialValue} plugins={plugins}  onChange={(x) => setValue(JSON.stringify(x))}>
+        <Toolbar>
+          <AlignToolbarButtons></AlignToolbarButtons>
+        </Toolbar>
+        <Plate
+          // plugins={plugins}
+          editableProps={editableProps}
+         
+        ></Plate>
+      </PlateProvider>
       <span>Raw value:</span>
       <span>{value}</span>
     </div>
